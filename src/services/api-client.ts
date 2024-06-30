2734bae6a5e93b0d8ed6fd73124e72586968659d
Fuad <fuadadio75@gmail.com>
2024-06-30 13:56:33 +0100

    feat: creates custom hook to fetch games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-	baseURL: "https://api.rawg.io/api/",
+	baseURL: "https://api.rawg.io/api",
 	params: {
 		key: "82b31456dabb465bb13efa347e7a2cda",
 	},
