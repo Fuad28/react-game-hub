c16adfadc9702218172c8d02f52f50cd1527d314
Fuad <fuadadio75@gmail.com>
2024-07-01 00:12:07 +0100

    feat: adds generics displays

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,13 +1,8 @@
 import useData from "./useData";
-
-export interface Platform {
-	id: number;
-	name: string;
-	slug: string;
-}
 export interface Genre {
 	id: number;
 	name: string;
+	image_background: string;
 }
 
 const useGenres = () => useData<Genre>("/genres");
